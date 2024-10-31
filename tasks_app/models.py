3191cc81c8f238a740ac8af1a6f6f6a7e0574708
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-10-31 21:28:09 +0100

    model tasks

--- tasks_app/models.py
+++ tasks_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class task(models.Model):
+    title = models.CharField(max_length=255)
+    taskStatus = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)  # add_task1 save()
+    assigned = models.CharField(max_length=255)
+    dueDate = models.CharField(max_length=255)
+    priorityContentArray = models.CharField(max_length=255)
+    subtasks = models.CharField(max_length=255)
+    tasks = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    subT = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class contact(models.Model):
+    title = models.CharField(max_length=255)
+    taskStatus = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.title
