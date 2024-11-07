e2c73f0ad66c23a59b276313f5d5a9eb707abb77
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-11-07 21:02:03 +0100

    update and delete contacts

--- tasks_app/api/serializer.py
+++ tasks_app/api/serializer.py
@@ -2,16 +2,19 @@ from rest_framework import serializers
 from tasks_app.models import Task, UserContact
 
 class TasksSerializer(serializers.ModelSerializer):
+    pk = serializers.ReadOnlyField() 
     class Meta:
         model = Task
         fields = '__all__'
 
 class ContactSerializer(serializers.ModelSerializer):
+    pk = serializers.ReadOnlyField() 
     class Meta:
         model = UserContact
         fields = '__all__'
 
 class AssignedContactSerializer(ContactSerializer, serializers.ModelSerializer):
+    pk = serializers.ReadOnlyField() 
     class Meta:
         model = UserContact
         exclude = ['email']
